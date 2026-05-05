80da44109dc11e2e7ba42ac4a0c4a53b4205d9f6
adil2133-git <adilaadi340@gmail.com>
2026-05-05 13:54:41 +0530

    loginController

--- library/controllers/adminController.js
+++ library/controllers/adminController.js
@@ -23,30 +23,39 @@ const registerController = async (req, res) => {
             password: hashedPassword
         })
 
-        return res.status(200).json({
+        res.status(200).json({
             message: "Admin registered successfully",
             newAdmin
         })
     } catch (err) {
-        return res.status(500).json({
+        res.status(500).json({
             message: "Admin registration failed",
             Error: err.message
         })
     }
 }
 
-// const loginController = async (req, res) => {
-//     try{
-//         const {username, password} = req.body
 
-//         if(!username || !password){
-//             return res.status(401).json({message:"all fields are required"})
-//         }
+const loginController = async (req, res) => {
+    try{
+        const {username, password} = req.body
 
+        const adminCheck = await adminModel.findOne({ username })
 
-//     }catch(err){
+        if(!adminCheck){
+            return res.status(404).json({message: "Admin not found"})
+        }
 
-//     }
-// }
+        const passwordCheck = await bcrypt.compare(password, adminCheck.password)
 
-module.exports = { registerController }
+        if(!passwordCheck){
+            return res.status(400).json({message: "Invalid password"})
+        }
+
+        res.status(200).json({message:"Login successful"})
+    }catch(err){
+        res.status(500).json({message: "Login error", Error: err.message})
+    }
+}
+
+module.exports =  {registerController, loginController} 
--- library/server.js
+++ library/server.js
@@ -3,10 +3,13 @@ require("dotenv").config()
 const app = express()
 
 const adminRoutes = require("./routes/adminRouter")
+const connectDB = require("./config/db")
 
 app.use(express.json())
 
 app.use("/api/auth", adminRoutes)
+
+connectDB()
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
