fd5845d0b6387ed18a44277efc0ec38743c62fc6
adil2133-git <adilaadi340@gmail.com>
2026-05-05 13:56:11 +0530

    login route setup

--- library/routes/adminRouter.js
+++ library/routes/adminRouter.js
@@ -3,7 +3,9 @@ const express = require("express")
 const router = express.Router()
 
 const registerController = require("../controllers/adminController")
+const loginController = require("../controllers/adminController")
 
 router.post("/register", registerController)
+router.post("/login", loginController)
 
 module.exports = router
