9e6d430119b0e4ae9a7b1c29bb98f482c1cb45e5
adil2133-git <adilaadi340@gmail.com>
2026-05-06 12:32:13 +0530

    book update

--- library/controllers/bookController.js
+++ library/controllers/bookController.js
@@ -31,17 +31,66 @@ const addBook = async (req, res) => {
 
 
 const getAllBooks = async (req, res) => {
-    try{
+    try {
         const books = await bookModel.find()
 
-        if(!books){
-            return res.status(404).json({message: "No book found"})
+        if (!books) {
+            return res.status(404).json({ message: "No book found" })
         }
 
-        res.status(200).json({message: "BOOKS fetched successfully", books})
-    }catch(err){
-        res.status(500).json({message: "Error while fetching book", Error: err.message})
+        res.status(200).json({ message: "BOOKS fetched successfully", books })
+    } catch (err) {
+        res.status(500).json({ message: "Error while fetching book", Error: err.message })
     }
 }
 
-module.exports = { addBook, getAllBooks }
+const getBookById = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const book = await bookModel.findById(id)
+
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" })
+        }
+
+        res.status(200).json({ message: "Book fetched successfully", book })
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching a Book", Error: err.message })
+    }
+}
+
+const updateBook = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const { title, author, genre, publishedYear } = req.body
+
+        const book = await bookModel.findById(id)
+
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" })
+        }
+
+        const updatedBook = await bookModel.findByIdAndUpdate(
+            id,
+            {
+
+                title,
+                author,
+                genre,
+                publishedYear
+            }
+        )
+
+    if (!updatedBook) {
+        return res.status(404).json({ message: "Book not found" })
+    }
+
+    res.status(200).json({ message: "Book updated successfully", updatedBook })
+}catch (err) {
+    res.status(500).json({ message: "Error updating book", Error: err.message })
+}
+}
+
+module.exports = { addBook, getAllBooks, getBookById, updateBook }
--- library/routes/bookRoutes.js
+++ library/routes/bookRoutes.js
@@ -3,9 +3,11 @@ const express = require("express")
 const router = express.Router()
 
 const auth = require("../middleware/authMiddleware")
-const {addBook, getAllBooks} = require("../controllers/bookController")
+const {addBook, getAllBooks, getBookById, updateBook} = require("../controllers/bookController")
 
 router.post("/add", auth, addBook)
 router.get("/books", auth, getAllBooks)
+router.get("/book/:id", auth, getBookById)
+router.put("/book/:id", auth, updateBook)
 
 module.exports = router
