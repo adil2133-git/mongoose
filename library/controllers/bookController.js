9c0d4cff8656d12debef5fe42ec2664b02b88cfa
adil2133-git <adilaadi340@gmail.com>
2026-05-06 10:34:55 +0530

    getAllbooks

--- library/controllers/bookController.js
+++ library/controllers/bookController.js
@@ -29,4 +29,19 @@ const addBook = async (req, res) => {
     }
 }
 
-module.exports = { addBook }
+
+const getAllBooks = async (req, res) => {
+    try{
+        const books = await bookModel.find()
+
+        if(!books){
+            return res.status(404).json({message: "No book found"})
+        }
+
+        res.status(200).json({message: "BOOKS fetched successfully", books})
+    }catch(err){
+        res.status(500).json({message: "Error while fetching book", Error: err.message})
+    }
+}
+
+module.exports = { addBook, getAllBooks }
--- library/routes/bookRoutes.js
+++ library/routes/bookRoutes.js
@@ -3,8 +3,9 @@ const express = require("express")
 const router = express.Router()
 
 const auth = require("../middleware/authMiddleware")
-const {addBook} = require("../controllers/bookController")
+const {addBook, getAllBooks} = require("../controllers/bookController")
 
 router.post("/add", auth, addBook)
+router.get("/books", auth, getAllBooks)
 
 module.exports = router
