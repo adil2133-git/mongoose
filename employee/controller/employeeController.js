2d567b3a927b94acb0351a43efe1281ee3293c2c
adil2133-git <adilaadi340@gmail.com>
2026-05-17 14:02:52 +0530

    get all employees done

--- employee/controller/employeeController.js
+++ employee/controller/employeeController.js
@@ -25,4 +25,18 @@ const addEmployee = async (req, res) => {
     }
 }
 
-module.exports = { addEmployee }
+const getEmployees = async (req, res) => {
+    try{
+        const employees = await employeeSchema.find()
+
+        if(!employees){
+            return res.status(404).json({message:"Employees not found"})
+        }
+
+        res.status(200).json({message: "Employees", employees})
+    }catch(err){
+        res.status(500).json({message: "getEmployees failed", Error: err.message})
+    }
+}
+
+module.exports = { addEmployee, getEmployees }
--- employee/routes/employeeRoute.js
+++ employee/routes/employeeRoute.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const { addEmployee } = require("../controller/employeeController")
+const { addEmployee, getEmployees } = require("../controller/employeeController")
 const protectRoute = require("../middleware/protectRotue")
 
 const router = express.Router()
 
 router.post("/add", protectRoute, addEmployee)
+router.get("/", protectRoute, getEmployees)
 
 module.exports = router
