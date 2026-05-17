f25b0695bd58bc8477a63a2e4b17741bdf8f63da
adil2133-git <adilaadi340@gmail.com>
2026-05-17 15:05:44 +0530

    delete employee done

--- employee/controller/employeeController.js
+++ employee/controller/employeeController.js
@@ -29,7 +29,7 @@ const getEmployees = async (req, res) => {
     try{
         const employees = await employeeSchema.find()
 
-        if(!employees){
+        if(employees.length === 0){
             return res.status(404).json({message:"Employees not found"})
         }
 
@@ -43,7 +43,7 @@ const getSingleEmployee = async (req, res) => {
     try{
         const {id} = req.params.id
 
-        const employee = await employeeSchema.find({id})
+        const employee = await employeeSchema.findById(id)
 
         if(!employee){
             return res.status(404).json({message: "Employee not found"})
@@ -89,4 +89,22 @@ const updateEmployee = async (req, res) => {
     }
 }
 
-module.exports = { addEmployee, getEmployees, getSingleEmployee, updateEmployee }
+const deleteEmployee = async (req, res) => {
+    try{
+        const {id} = req.params
+
+        const employee = await employeeSchema.findById(id)
+
+        if(!employee){
+            return res.status(404).json("Employee not found")
+        }
+
+        const delEmployee = await employeeSchema.findByIdAndDelete(id)
+
+        res.status(200).json({message: "Employee Deleted successfully", delEmployee})
+    }catch(err){
+        res.status(500).json({message: "Employee deletion failed", Error: err.message})
+    }
+}
+
+module.exports = { addEmployee, getEmployees, getSingleEmployee, updateEmployee, deleteEmployee }
--- employee/routes/employeeRoute.js
+++ employee/routes/employeeRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { addEmployee, getEmployees, getSingleEmployee, updateEmployee } = require("../controller/employeeController")
+const { addEmployee, getEmployees, getSingleEmployee, updateEmployee, deleteEmployee } = require("../controller/employeeController")
 const protectRoute = require("../middleware/protectRotue")
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.post("/add", protectRoute, addEmployee)
 router.get("/", protectRoute, getEmployees)
 router.get("/:id", protectRoute, getSingleEmployee)
 router.put("/:id", protectRoute, updateEmployee)
+router.delete("/:id", protectRoute, deleteEmployee)
 
 module.exports = router
