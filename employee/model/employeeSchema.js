ea46a114a0332b31d1526ab31426798a9d1ff5ab
adil2133-git <adilaadi340@gmail.com>
2026-05-17 13:05:14 +0530

    employee Schema created

---
+++ employee/model/employeeSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const employeeSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    department:{
+        type: String,
+        required: true
+    },
+    salary:{
+        type: Number,
+        required: true
+    },
+    joiningDate:{
+        type: Date,
+        default: Date.now()
+    }
+}, {timestamp: true})
+
+module.exports = employeeSchema
