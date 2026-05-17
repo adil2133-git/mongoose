df2a4568be09bfe76f1e5717c91f2b7631224fdc
adil2133-git <adilaadi340@gmail.com>
2026-05-17 11:46:50 +0530

    error msg added

--- employee/controller/admin.js
+++ employee/controller/admin.js
@@ -13,7 +13,7 @@ const loginController = (req, res) => {
 
         res.status(200).json({message: "Login successful", token})
     }catch(err){
-        res.status(500).json({message:"Login Failed"})
+        res.status(500).json({message:"Login Failed", Error:err.message})
     }
 }
 
